7fd246ce0b1cae61a9677b2b9ae2f8f5dfa9aa41
Devi Prasad <dp@vlabs.ac.in>
2014-01-21 23:01:57 +0530

    small enhancements

--- lectures/fourth(Jan-11)/gameserver.py
+++ lectures/fourth(Jan-11)/gameserver.py
@@ -3,44 +3,56 @@
 import BaseHTTPServer
 import SocketServer
 
+httpd = None
+keep_running = True
+
 class GameServer(BaseHTTPServer.BaseHTTPRequestHandler):
+    TCP_PORT = 54321
+
     def do_GET(self):
-        print "do get handler!"
         print "path: ", self.path
-        print "request: ", self.request
-        print "raw_requestline: ", self.raw_requestline
-        print "address_string: ", self.address_string
-        print "serveRequest: ", self.serveRequest
-        print "server: ", self.serveRequest
 
-        self.serveRequest()
-
-    def do_HEAD(self):
-        print "do head handler!"
         self.serveRequest()
 
     def serveRequest(self):
     	resource = self.path.lower()
+        response = 200
     	if resource == "/": 
 	        body = "<html><head></head><body>Empty Reesponse</body></html>"
-        elif resource == "/weapon/arrow":
-        	body = "<html><head></head><body>{weapon: 'arrow', type: 'lethal', range: '5'}</body></html>"
-        elif 
+        elif resource == "/weapon/arrow" or resource ==  "/weapon/arrow/":
+        	body = "<html><head></head><body>{weapon: 'arrow', type: 'lethal', range: '10'}</body></html>"
+        elif resource == "/stop" or resource ==  "/stop/":
+            print "will stop next!!"
+            global keep_running
+            keep_running = False
+
+            body=""
         else:
-        	body = "<html><head></head><body><h1>Unknown Request!</h1></body></html>"
-       
-        self.send_response(200)
+            body = "<html><head></head><body><h1>Unknown Request!</h1></body></html>"
+            response = 400
+
+        self.send_response(response)
         self.send_header("Content-type", "text/html")
         self.send_header("Content-Length", len(body))
         self.end_headers()
         self.wfile.write(body)
-        
 
-PORT = 65432
 
-Handler = GameServer
+SocketServer.TCPServer.allow_reuse_address = True
+httpd = SocketServer.TCPServer(("127.0.0.1", GameServer.TCP_PORT), GameServer)
+#httpd = SocketServer.TCPServer(("192.168.2.13", GameServer.TCP_PORT), GameServer)
+keep_running = True
 
-httpd = SocketServer.TCPServer(("127.0.0.1", PORT), Handler)
+print "serving at port", GameServer.TCP_PORT
+while keep_running:
+    httpd.handle_request()
 
-print "serving at port", PORT
-httpd.serve_forever()
+exit(0)
+
+
+
+
+
+
+
+
