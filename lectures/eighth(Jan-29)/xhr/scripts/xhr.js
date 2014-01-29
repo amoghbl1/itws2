0f5a81ebdaa95937f11dd36818f4d633f5011409
Devi Prasad <dp@vlabs.ac.in>
2014-01-29 11:31:39 +0530

    added adequate whitespaces

--- lectures/eighth(Jan-29)/xhr/scripts/xhr.js
+++ lectures/eighth(Jan-29)/xhr/scripts/xhr.js
@@ -1,18 +1,18 @@
 utils = {
     getQuizzes: function() {
 	    var xhr = new XMLHttpRequest()
-	    xhr.withCredentials = true;
 	    console.log("utils::getQuizzes")
-	    xhr.onload = function() {
+	    
+        xhr.onload = function() {
 	        console.log("xhr::onload: ", this.responseText);
 	    }
 
 	    xhr.open("GET", "http://127.0.0.1:54321/quizzes", true)
 	    
-	    xhr.onerror = function(e) {
+	    xhr.onerror = (function(e) {
 	    	console.log("xhr::send error: " +  e)
-            utils.reportError(e)
-        }
+            this.reportError(e)
+        }).bind()
 
         xhr.send()
     },
